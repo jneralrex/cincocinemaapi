7727b1dbd390911676bc53aab309c1daf59fbce4
Idah Abubakar Isah <idahabubakar49@gmail.com>
2025-01-14 18:52:31 +0100

    correcting deployment error

--- models/screen.model.js
+++ models/screen.model.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
-<<<<<<< HEAD
 const screenSchema = new mongoose.Schema(
-=======
-const screenSchema = mongoose(
->>>>>>> 11ec757e9a50c975a2ea83e3d252f1304fcaa2eb
   {
     screenName: {
       type: String,
