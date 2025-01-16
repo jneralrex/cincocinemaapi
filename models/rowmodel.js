8c7ef40cfc8ee8fc402824a1fa8bfa002be5b767
AYOMIDE-LIADI <167301347+AYOMIDE-LIADI@users.noreply.github.com>
2025-01-16 05:46:33 -0800

    made another minor change

--- models/rowmodel.js
+++ models/rowmodel.js
@@ -20,19 +20,16 @@ const rowSchema = new mongoose.Schema({
   },
 });
 
-// Pre-save middleware to update totalSeats dynamically
 rowSchema.pre('save', function (next) {
-  // Calculate total seats based on the seatIds array length
   this.totalSeats = this.seatIds.length;
   next();
 });
 
-// Pre-update middleware for `findOneAndUpdate` and similar operations
 rowSchema.pre('findOneAndUpdate', async function (next) {
   const update = this.getUpdate();
   if (update && update.$set && update.$set.seatIds) {
     const totalSeats = update.$set.seatIds.length;
-    update.$set.totalSeats = totalSeats; // Dynamically set totalSeats
+    update.$set.totalSeats = totalSeats; 
   }
   next();
 });
