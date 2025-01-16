139b6b17db34b3f2e6574538a7477d44ed4199a2
Idah Abubakar Isah <idahabubakar49@gmail.com>
2025-01-16 07:00:43 +0100

    PATCH added to cors

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true, 
 };
 
