ea99063e6dee2ab224f671bb72bc9b437c00d691
Emmanuel-Kola24 <167301908+Emmanuel-Kola24@users.noreply.github.com>
2025-01-23 13:18:33 +0100

    Date model and Time model adjusted to accept  only dates and only time respectively

--- models/date.model.js
+++ models/date.model.js
@@ -6,27 +6,12 @@ const dateSchema = new mongoose.Schema({
     ref: "movie",
     required: [true, "Movie ID is required"],
   },
-  screeningDates: [{
+  screeningDates: {
     date: {
       type: Date,
       required: [true, "Screening date is required"]
     },
-    showTimes: [{
-      time: {
-        type: String,
-        required: [true, "Show time is required"]
-      },
-      screenId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Screen",
-        required: [true, "Screen ID is required"]
-      },
-      availableSeats: {
-        type: Number,
-        required: [true, "Available seats count is required"]
-      }
-    }]
-  }],
+  },
   isActive: {
     type: Boolean,
     default: true
--- models/time.model.js
+++ models/time.model.js
@@ -11,23 +11,13 @@ const timeSchema = new mongoose.Schema({
     ref: "Date",
     required: [true, "Date ID is required"],
   },
-  showTimes: [
+  showTimes: 
     {
       time: {
         type: String,
         required: [true, "Show time is required"],
       },
-      screenId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Screen",
-        required: [true, "Screen ID is required"],
-      },
-      availableSeats: {
-        type: Number,
-        required: [true, "Available seats count is required"],
-      },
-    },
-  ],
+     },
   isActive: {
     type: Boolean,
     default: true,
